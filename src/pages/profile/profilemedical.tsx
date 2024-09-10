86d693e1d0d577914a4fc83c4b53a782a7391950
phangiathuyen <thuyendi2004@gmail.com>
2024-09-10 07:57:38 -0700

    Normalize line endings

---
+++ src/pages/profile/profilemedical.tsx
@@ -0,0 +1,8 @@
+// src/pages/profile/profilemedical.jsx
+import React from 'react';
+
+const ProfileMedical = () => {
+  return <div>Profile Medical</div>;
+};
+
+export default ProfileMedical;
---
+++ src/routes/index.tsx
@@ -0,0 +1,273 @@
+import React from "react";
+import { Route, Navigate, RouteProps } from "react-router-dom";
+
+// components
+import PrivateRoute from "./PrivateRoute";
+// import Root from './Root';
+
+// lazy load all the views
+
+// auth
+const Login = React.lazy(() => import("../pages/auth/Login"));
+const Logout = React.lazy(() => import("../pages/auth/Logout"));
+const Confirm = React.lazy(() => import("../pages/auth/Confirm"));
+const ForgetPassword = React.lazy(() => import("../pages/auth/ForgetPassword"));
+const Register = React.lazy(() => import("../pages/auth/Register"));
+const SignInSignUp = React.lazy(() => import("../pages/auth/SignInSignUp"));
+const LockScreen = React.lazy(() => import("../pages/auth/LockScreen"));
+
+const Login2 = React.lazy(() => import("../pages/auth2/Login2"));
+const Logout2 = React.lazy(() => import("../pages/auth2/Logout2"));
+const Register2 = React.lazy(() => import("../pages/auth2/Register2"));
+const Confirm2 = React.lazy(() => import("../pages/auth2/Confirm2"));
+const ForgetPassword2 = React.lazy(
+  () => import("../pages/auth2/ForgetPassword2")
+);
+const LockScreen2 = React.lazy(() => import("../pages/auth2/LockScreen2"));
+const SignInSignUp2 = React.lazy(() => import("../pages/auth2/SignInSignUp2"));
+
+// landing
+const Landing = React.lazy(() => import("../pages/landing/"));
+
+// dashboard
+const Dashboard1 = React.lazy(() => import("../pages/dashboard/Dashboard1/"));
+const Medicalexamination = React.lazy(() => import("../pages/medical/index"));
+const ProfileMedical = React.lazy(() => import("../pages/profile/profilemedical"));
+
+const Error404 = React.lazy(() => import("../pages/error/Error404"));
+const Error404Two = React.lazy(() => import("../pages/error/Error404Two"));
+const Error500 = React.lazy(() => import("../pages/error/Error500"));
+const Error500Two = React.lazy(() => import("../pages/error/Error500Two"));
+
+const Upcoming = React.lazy(() => import("../pages/other/Upcoming"));
+
+const Maintenance = React.lazy(() => import("../pages/other/Maintenance"));
+
+// uikit
+const Buttons = React.lazy(() => import("../pages/uikit/Buttons"));
+
+
+
+
+export interface RoutesProps {
+  path: RouteProps["path"];
+  name?: string;
+  element?: RouteProps["element"];
+  route?: any;
+  exact?: boolean;
+  icon?: string;
+  header?: string;
+  roles?: string[];
+  children?: RoutesProps[];
+}
+
+// root routes
+// const rootRoute: RoutesProps = {
+//     path: '/',
+//     exact: true,
+//     element: () => <Root />,
+//     route: Route,
+// };
+
+// dashboards
+const dashboardRoutes: RoutesProps = {
+  path: "/dashboard",
+  name: "Dashboards",
+  icon: "airplay",
+  header: "Navigation",
+  children: [
+    {
+      path: "/",
+      name: "Root",
+      element: <Navigate to="/dashboard-1" />,
+      route: PrivateRoute,
+    },
+    {
+      path: "/dashboard-1",
+      name: "Dashboard 1",
+      element: <Dashboard1 />,
+      route: PrivateRoute,
+    },
+    {
+      path: "/medical-examination",
+      name: "medical-examination",
+      element: <Medicalexamination />,
+      route: PrivateRoute,
+    },
+    {
+      path: "/profile-medical",
+      name: "profile-medical",
+      element: <ProfileMedical />,
+      route: PrivateRoute,
+    }
+  
+  ],
+};
+
+
+
+// auth
+const authRoutes: RoutesProps[] = [
+  {
+    path: "/auth/login",
+    name: "Login",
+    element: <Login />,
+    route: Route,
+  },
+  
+  {
+    path: "/auth/register",
+    name: "Register",
+    element: <Register />,
+    route: Route,
+  },
+  {
+    path: "/auth/confirm",
+    name: "Confirm",
+    element: <Confirm />,
+    route: Route,
+  },
+  {
+    path: "/auth/forget-password",
+    name: "Forget Password",
+    element: <ForgetPassword />,
+    route: Route,
+  },
+  {
+    path: "/auth/signin-signup",
+    name: "SignIn-SignUp",
+    element: <SignInSignUp />,
+    route: Route,
+  },
+  {
+    path: "/auth/lock-screen",
+    name: "Lock Screen",
+    element: <LockScreen />,
+    route: Route,
+  },
+  {
+    path: "/auth/logout",
+    name: "Logout",
+    element: <Logout />,
+    route: Route,
+  },
+  {
+    path: "/auth/login2",
+    name: "Login2",
+    element: <Login2 />,
+    route: Route,
+  },
+  {
+    path: "/auth/logout2",
+    name: "Logout2",
+    element: <Logout2 />,
+    route: Route,
+  },
+  {
+    path: "/auth/register2",
+    name: "Register2",
+    element: <Register2 />,
+    route: Route,
+  },
+  {
+    path: "/auth/confirm2",
+    name: "Confirm2",
+    element: <Confirm2 />,
+    route: Route,
+  },
+  {
+    path: "/auth/forget-password2",
+    name: "Forget Password2",
+    element: <ForgetPassword2 />,
+    route: Route,
+  },
+  {
+    path: "/auth/signin-signup2",
+    name: "SignIn-SignUp2",
+    element: <SignInSignUp2 />,
+    route: Route,
+  },
+  {
+    path: "/auth/lock-screen2",
+    name: "Lock Screen2",
+    element: <LockScreen2 />,
+    route: Route,
+  },
+];
+
+// public routes
+const otherPublicRoutes = [
+  {
+    path: "/landing",
+    name: "landing",
+    element: <Landing />,
+    route: Route,
+  },
+  {
+    path: "/maintenance",
+    name: "Maintenance",
+    element: <Maintenance />,
+    route: Route,
+  },
+  {
+    path: "/error-404",
+    name: "Error - 404",
+    element: <Error404 />,
+    route: Route,
+  },
+  {
+    path: "/error-404-two",
+    name: "Error - 404 Two",
+    element: <Error404Two />,
+    route: Route,
+  },
+  {
+    path: "/error-500",
+    name: "Error - 500",
+    element: <Error500 />,
+    route: Route,
+  },
+  {
+    path: "/error-500-two",
+    name: "Error - 500 Two",
+    element: <Error500Two />,
+    route: Route,
+  },
+  {
+    path: "/upcoming",
+    name: "Coming Soon",
+    element: <Upcoming />,
+    route: Route,
+  },
+];
+
+// flatten the list of all nested routes
+const flattenRoutes = (routes: RoutesProps[]) => {
+  let flatRoutes: RoutesProps[] = [];
+
+  routes = routes || [];
+  routes.forEach((item: RoutesProps) => {
+    flatRoutes.push(item);
+
+    if (typeof item.children !== "undefined") {
+      flatRoutes = [...flatRoutes, ...flattenRoutes(item.children)];
+    }
+  });
+  return flatRoutes;
+};
+
+// All routes
+const authProtectedRoutes = [
+  dashboardRoutes,
+ 
+];
+const publicRoutes = [...authRoutes, ...otherPublicRoutes];
+
+const authProtectedFlattenRoutes = flattenRoutes([...authProtectedRoutes]);
+const publicProtectedFlattenRoutes = flattenRoutes([...publicRoutes]);
+export {
+  publicRoutes,
+  authProtectedRoutes,
+  authProtectedFlattenRoutes,
+  publicProtectedFlattenRoutes,
+};
